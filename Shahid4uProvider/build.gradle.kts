f025087ba4fb0c1ca5845d867280137835fa307f
Reham1071987 <127853743+Reham1071987@users.noreply.github.com>
2023-04-30 23:39:56 +0200

    Update build.gradle.kts

--- Shahid4uProvider/build.gradle.kts
+++ Shahid4uProvider/build.gradle.kts
@@ -9,5 +9,5 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=shahid4u.cc&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=shahed-4u.net&sz=%size%"
 }
